627092acbbe1bd980ee65faeb922283e597a5fa9
djarkan <36413985+djarkan@users.noreply.github.com>
2023-05-13 17:01:12 +0200

    Add files via upload

--- triple/triple.hpp
+++ triple/triple.hpp
@@ -1,1 +1,26 @@
+namespace mylib
+{
+template <typename T, typename U,typename V>
+class Triple
+{
+   public:
+        Triple();
+        Triple(T first, U second, V third);
 
+        T first;
+        U second;
+        V third;
+};
+template <typename T, typename U,typename V>
+Triple<T, U, V>::Triple()
+{
+
+}
+
+template <typename T, typename U,typename V>
+Triple<T, U, V>::Triple(T first, U second, V third) : first{first}, second{second}, third{third}
+{
+
+}
+
+}
