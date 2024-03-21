3100b3e83bbad4b337d6ac9fa5c2ae1c128e1ad9
djarkan <36413985+djarkan@users.noreply.github.com>
2024-03-21 22:31:44 +0100

    Add files via upload

---
+++ text/TTFtext/ttftext.cpp
@@ -0,0 +1,260 @@
+#include "ttftext.hpp"
+
+#include <iostream>
+
+namespace mylib
+{
+
+TTFtext::TTFtext(sf::RenderWindow& window): m_fontSize{10}, m_color{sf::Color::White}, m_outlinedColor{sf::Color::White}, m_window{window}
+{
+
+}
+
+TTFtext::TTFtext(const std::string& text, const std::string& fontName, unsigned int fontSize, sf::Color color, sf::RenderWindow& window) : m_string{text}, m_fontSize{fontSize}, m_color{color}, m_window{window}
+{
+    m_font.loadFromFile(fontName);
+    m_text.setFont(m_font);
+}
+
+bool TTFtext::setFont(const std::string& fontName)
+{
+    bool fontOK = m_font.loadFromFile(fontName);
+    m_text.setFont(m_font);
+    return fontOK;
+}
+
+void TTFtext::setText(const std::string& text)
+{
+    m_string = text;
+}
+
+void TTFtext::setColor(const sf::Color color)
+{
+    m_text.setFillColor(color);
+}
+
+void TTFtext::setOutlineColor(const sf::Color color)
+{
+    m_text.setOutlineColor(color);
+}
+
+void TTFtext::setCharacterSize(const unsigned int charSize)
+{
+    m_text.setCharacterSize(charSize);
+    m_fontSize = charSize;
+}
+
+void TTFtext::displayEnrichedText(float x, float y)
+{
+    float oldx = x;
+    float oldY = y;
+    m_text.setPosition(x, y);
+    m_text.setStyle(sf::Text::Regular);
+    for(unsigned int i = 0; i < m_string.size(); ++i) {
+        if(m_string[i] != '[') {
+            if(m_string[i] == '\n') {
+                x = oldx;
+                y += m_text.getCharacterSize() + 4;
+                m_text.setPosition(x, y);
+            }
+            else { displayChar(x, y , m_fontSize, m_string[i]); }
+        }
+        else {                                                                                                           // enter in a boudary
+            ++i;
+            char letter = m_string[i];
+            bool addStyle;                                                                                               // add or remove text style
+            sf::Uint32 style = m_text.getStyle();
+            if(letter != '/') { addStyle = true; }
+            else {
+                    addStyle =  false;
+                    ++i;
+                    letter = m_string[i];
+            }
+            if(letter == 'r' || letter == 'b' || letter == 'i' || letter == 'u' || letter == 't') {
+                if(m_string[i + 1] == ']') {
+                    switch (letter) {
+                        case 'r':                                                                                        // regular : remove all styles [r] or [/r]  only one is enough
+                            m_text.setStyle(style& 0b00000000);
+                            m_text.setCharacterSize(m_fontSize);
+                            m_text.setFillColor(m_color);
+                            break;
+                        case 'b':                                                                                        // bold
+                            if(addStyle) { m_text.setStyle(style | sf::Text::Bold); }
+                            else { m_text.setStyle(style & 0b11111110); }
+                            break;
+                        case 'i':                                                                                        // italic
+                            if(addStyle) { m_text.setStyle(style | sf::Text::Italic); }
+                            else { m_text.setStyle(style & 0b11111101); }
+                            break;
+                        case 'u':                                                                                        // underlined
+                            if(addStyle) { m_text.setStyle(style | sf::Text::Underlined); }
+                            else { m_text.setStyle(style & 0b11111011); }
+                            break;
+                        case 't':                                                                                       // StrikeThrough
+                            if(addStyle) { m_text.setStyle(style | sf::Text::StrikeThrough); }
+                            else { m_text.setStyle(style & 0b11110111); }
+                            break;
+                        default:
+                            break;
+                    }
+                    ++i;
+                }
+                else {
+                    if(addStyle) {
+                        displayChar(x, y , m_fontSize,m_string[i - 1]);
+                        displayChar(x, y , m_fontSize,m_string[i]);
+                    }
+                    else {
+                        displayChar(x, y , m_fontSize,m_string[i - 2]);
+                        displayChar(x, y , m_fontSize,m_string[i - 1]);
+                        displayChar(x, y , m_fontSize,m_string[i]);
+                    }
+                }
+            }
+            else {
+                if(letter == 's' or letter == 'c') {
+                    if((addStyle && m_string[i + 1] == ']') || m_string[i + 2] == ']' || m_string[i + 3] == ']' || m_string[i + 4] == ']' ) {
+                        std::string numberSize{""};
+                        ++i;
+                        int offset{0};
+                        bool isNumericDigit{true};
+                        unsigned int supposedSizeLength{0};
+                        while(m_string[i + supposedSizeLength] != ']') { ++supposedSizeLength; }
+                        while(m_string[i + offset] != ']' && offset < 3 && isNumericDigit && m_string[i + offset] != ']') {
+                            if(std::isdigit(m_string[i + offset])) {
+                                numberSize.push_back(m_string[i + offset]);
+                                ++offset;
+                            }
+                            else { isNumericDigit = false; }
+                        }
+                        if(numberSize.size() == supposedSizeLength && supposedSizeLength > 0) {
+                            unsigned int number = std::stoi(numberSize);
+                            i += numberSize.size();
+                            if(letter == 's') {                                                                         // font size
+                                m_text.setCharacterSize(number);
+                                y = oldY;
+                                if(number > m_fontSize) { y -= (number - m_fontSize); std::cout << "number " << number << " offset Y " << number - m_fontSize << std::endl; }
+                                else { y += (m_fontSize - number);}
+                            }
+                            else { if(number <= 28 && number >= 0) { m_text.setFillColor(selectColor(number)); }}                      // color
+                        }
+                        else {
+                            displayChar(x, y , m_fontSize, m_string[i - 2]);
+                            displayChar(x, y , m_fontSize, m_string[i - 1]);
+                            displayChar(x, y , m_fontSize, m_string[i]);
+                        }
+                    }
+                    else {
+                        if(m_string[i + 1] == ']') {
+                            if(letter == 's'){
+                                y = oldY;
+                                m_text.setCharacterSize(m_fontSize);
+                            }
+                            else { m_text.setFillColor(m_color); }
+                            ++i;
+                        }
+                        else {
+                            displayChar(x, y , m_fontSize, m_string[i - 2]);
+                            displayChar(x, y , m_fontSize, m_string[i - 1]);
+                            displayChar(x, y , m_fontSize, m_string[i]);
+                        }
+                    }
+                }
+                else {
+                    std::string letters{"["};
+                    if(!addStyle) { letters.push_back(m_string['/']); }
+                    letters.push_back(m_string[i]);
+                    m_text.setString(letters);
+                    m_text.setPosition(x, y);
+                    m_window.draw(m_text);
+                    x +=  m_fontSize * (0.40 * letters.size());
+                }
+            }
+       }
+    }
+}
+
+sf::Color TTFtext::selectColor( int colorNb)
+{
+    switch (colorNb) {
+        case static_cast<int>(TextColor::Black):                                                // 0
+            return sf::Color::Black;
+        case static_cast<int>(TextColor::LightBrown):                                           // 1
+            return  static_cast<sf::Color>(0xE49467FF);
+        case static_cast<int>(TextColor::Brown):                                                // 2
+            return static_cast<sf::Color>(0xB97A56FF);
+        case static_cast<int>(TextColor::DarkBrown):                                            // 3
+            return static_cast<sf::Color>(0x805740FF);
+        case static_cast<int>(TextColor::LightRed):                                             // 4
+            return static_cast<sf::Color>(0xff343cFF);
+        case static_cast<int>(TextColor::Red):                                                  // 5
+            return static_cast<sf::Color>(0xe61a22FF);
+        case static_cast<int>(TextColor::DarkRed):                                              // 6
+            return static_cast<sf::Color>(0xeb1b21FF);
+        case static_cast<int>(TextColor::LightPink):                                            // 7
+            return static_cast<sf::Color>(0xffacc7FF);
+        case static_cast<int>(TextColor::Pink):                                                 // 8
+            return static_cast<sf::Color>(0xff8eb2FF);
+        case static_cast<int>(TextColor::DarkPink):                                             // 9
+            return static_cast<sf::Color>(0xff6496FF);
+        case static_cast<int>(TextColor::LightOrange):                                          // 10
+            return static_cast<sf::Color>(0xffa263FF);
+        case static_cast<int>(TextColor::Orange):                                               // 11
+            return static_cast<sf::Color>(0xff7f27FF);
+        case static_cast<int>(TextColor::DarkOrange):                                           // 12
+            return static_cast<sf::Color>(0xe67a2fFF);
+        case static_cast<int>(TextColor::LightYellow):                                          // 13
+            return static_cast<sf::Color>(0xfffbb0FF);
+        case static_cast<int>(TextColor::Yellow):                                               // 14
+            return static_cast<sf::Color>(0xfff200FF);
+        case static_cast<int>(TextColor::DarkYellow):                                           // 15
+            return static_cast<sf::Color>(0xd1c707FF);
+        case static_cast<int>(TextColor::LightBlue):                                            // 16
+            return static_cast<sf::Color>(0x4479ffFF);
+        case static_cast<int>(TextColor::Blue):                                                 // 17
+            return static_cast<sf::Color>(0x0048ffFF);
+        case static_cast<int>(TextColor::DarkBlue):                                             // 18
+            return static_cast<sf::Color>(0x0534abFF);
+        case static_cast<int>(TextColor::LightGreen):                                           // 19
+            return static_cast<sf::Color>(0x18df50FF);
+        case static_cast<int>(TextColor::Green):                                                // 20
+            return static_cast<sf::Color>(0x16b944FF);
+        case static_cast<int>(TextColor::DarkGreen):                                            // 21
+            return static_cast<sf::Color>(0x0a8b2eFF);
+        case static_cast<int>(TextColor::LightPurple):                                          // 22
+            return static_cast<sf::Color>(0xfb07ffFF);
+        case static_cast<int>(TextColor::Purple):                                               // 23
+            return static_cast<sf::Color>(0xcd2bcfFF) ;
+        case static_cast<int>(TextColor::DarkPurple):                                           // 24
+            return static_cast<sf::Color>(0xa120a3FF);
+        case static_cast<int>(TextColor::LightGrey):                                            // 25
+            return static_cast<sf::Color>(0xBBBBBBFF);
+        case static_cast<int>(TextColor::Grey):                                                 // 26
+            return static_cast<sf::Color>(0x999999FF);
+        case static_cast<int>(TextColor::DarkGrey):                                             // 27
+            return static_cast<sf::Color>(0x777777FF);
+        case static_cast<int>(TextColor::White):                                                // 28
+        default:
+            return sf::Color::White;
+    }
+    return sf::Color::White;
+}
+
+void TTFtext::displaySimpleText(const float x, const float y)
+{
+    const sf::Vector2f position(x,y);
+    m_text.setPosition(position);
+    m_text.setString(m_string);
+    m_window.draw(m_text);
+std::cout << m_string << std::endl;
+}
+
+void TTFtext::displayChar(float& x, const float y, const int charSize, const char printedChar)
+{
+    m_text.setString(printedChar);
+    m_text.setPosition(x, y);
+    m_window.draw(m_text);
+    x +=  m_text.getCharacterSize() * 0.4;
+}
+
+}
---
+++ text/TTFtext/ttftext.hpp
@@ -0,0 +1,60 @@
+#ifndef TTFTEXT_H
+#define TTFTEXT_H
+
+#include <SFML/Graphics/Color.hpp>
+#include <SFML/Graphics/RenderWindow.hpp>
+#include <SFML/Graphics/Text.hpp>
+                                                     /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+                                                     //                                                                                                                                             //
+                                                     //                                                                                                                                             //
+                                                     //                                    Display on screen a TTF font text of any size at coords x, y                                             //
+                                                     //                              text can be enriched with balises : "C[s120][c6]O[c8]U[c10][i][s55]C[c12]O[b][c14][u]U"                        //
+                                                     //                                                                                                                                             //
+                                                     //                     [/r] remove all balises to set previous settings.                                                                       //
+                                                     //                     [i] [/i] add or remove italic.                                                                                          //
+                                                     //                     [b] [/b] add or remove bold.                                                                                            //
+                                                     //                     [u] [/u] add or remove underlined.                                                                                      //
+                                                     //                     [t] [/t] add or remove striketrought.                                                                                   //
+                                                     //                     [sXXX] [/s] change or swap back the character size. XXX from 5 to 999.                                                  //
+                                                     //                     [cXX] [/c] change or swap back the color. XX from 0 to 28.                                                              //
+                                                     //                         Black = 0, LightBrown = 1, Brown = 2, DarkBrown = 3, LightRed = 4, Red = 5, DarkRed = 6,                            //
+                                                     //                         LightPink = 7, Pink = 8, DarkPink = 9, LightOrange = 10, Orange = 11, DarkOrange = 12,                              //
+                                                     //                         LightYellow = 13, Yellow = 14, DarkYellow = 15, LightBlue = 16, Blue = 17, DarkBlue = 18,                           //
+                                                     //                         LightGreen = 19, Green = 20, DarkGreen = 21, LightPurple = 22, Purple = 23, DarkPurple = 24,                        //
+                                                     //                         LightGrey = 25, Grey = 26, DarkGrey = 27, White = 28                                                                //
+                                                     //                                                                                                                                             //
+                                                     //                                                                                                                                             //
+                                                     /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+namespace mylib {
+
+class TTFtext
+{
+    public:
+                                        TTFtext(sf::RenderWindow& window);
+                                        TTFtext(const std::string& text, const std::string& fontName, unsigned int fontSize, sf::Color color, sf::RenderWindow& window);
+                 void                   setWindow(sf::RenderWindow& window);
+                 bool                   setFont(const std::string& fontName);
+                 void                   setText(const std::string& text);
+                 void                   setColor(const sf::Color color);
+                 void                   setOutlineColor(const sf::Color color);
+                 void                   setCharacterSize(const unsigned int charSize);
+                 void                   displayEnrichedText(float x, float y);
+                 void                   displaySimpleText(const float x, const float y);
+
+    private :
+                 enum class TextColor { Black = 0, LightBrown = 1, Brown = 2, DarkBrown = 3, LightRed = 4, Red = 5, DarkRed = 6, LightPink = 7, Pink = 8, DarkPink = 9, LightOrange = 10, Orange = 11, DarkOrange = 12, LightYellow = 13, Yellow = 14,
+                                        DarkYellow = 15, LightBlue = 16, Blue = 17, DarkBlue = 18, LightGreen = 19, Green = 20, DarkGreen = 21, LightPurple = 22, Purple = 23, DarkPurple = 24, LightGrey = 25, Grey = 26, DarkGrey = 27, White = 28};
+                 std::string            m_string;
+                 sf::Text               m_text;
+                 sf::Font               m_font;
+                 unsigned int           m_fontSize;
+                 sf::Color              m_color;
+                 sf::Color              m_outlinedColor;
+                 sf::RenderWindow&      m_window;
+
+                sf::Color              selectColor( int colorNb);
+                void                   displayChar(float& x, const float y, const int charSize, const char printedChar);
+};
+
+}
+#endif
